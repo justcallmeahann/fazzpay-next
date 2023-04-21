8cace6bfca53004440029ded19ace301aba147fb
justcallmeahann <80017838+justcallmeahann@users.noreply.github.com>
2023-04-21 11:43:43 +0700

    add link on sidebar

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,80 +1,138 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Sidebar() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+  const style = {
+    active: "border-primary stroke-primary text-primary  font-semibold",
+    inactive: "border-white stroke-dark",
+  };
   return (
     <aside className="relative">
       <div className="py-6 fixed mt-28 w-[16rem] bg-white rounded-3xl shadow-card-md h-[70vh] text-dark">
         <ul className="flex flex-col h-full">
-          <li className="border-primary border-l-4 px-6 my-5 flex items-center gap-4 text-primary font-semibold">
-            <svg
-              width="28"
-              height="28"
-              viewBox="0 0 28 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <li>
+            <Link
+              href={"/dashboard/"}
+              className={`${
+                currentRoute === "/dashboard" ? style.active : style.inactive
+              }  border-l-4 px-6 my-5 flex items-center gap-4 transition-colors hover:text-primary-focus   hover:stroke-primary-focus`}
             >
-              <path
-                d="M11.6667 3.5H3.5V11.6667H11.6667V3.5Z"
-                stroke="#6379F4"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M24.5 3.5H16.3333V11.6667H24.5V3.5Z"
-                stroke="#6379F4"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M24.5 16.333H16.3333V24.4997H24.5V16.333Z"
-                stroke="#6379F4"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M11.6667 16.333H3.5V24.4997H11.6667V16.333Z"
-                stroke="#6379F4"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            Dashboard
+              <svg
+                width="28"
+                height="28"
+                viewBox="0 0 28 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.6667 3.5H3.5V11.6667H11.6667V3.5Z"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M24.5 3.5H16.3333V11.6667H24.5V3.5Z"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M24.5 16.333H16.3333V24.4997H24.5V16.333Z"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M11.6667 16.333H3.5V24.4997H11.6667V16.333Z"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              Dashboard
+            </Link>
           </li>
-          <li className="border-white border-l-4 px-6 my-5 flex items-center gap-4">
-            <svg
-              width="28"
-              height="28"
-              viewBox="-5 1 28 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+
+          <li>
+            <Link
+              href={"/dashboard/transfer"}
+              className={`${
+                currentRoute === "/dashboard/transfer"
+                  ? style.active
+                  : style.inactive
+              }  border-l-4 px-6 my-5 flex items-center gap-4 transition-colors hover:text-primary-focus   hover:stroke-primary-focus`}
             >
-              <path
-                d="M10 18.1663V1.83301"
-                stroke="#3A3D42"
-                stroke-opacity="0.8"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M1.83333 9.99968L10 1.83301L18.1667 9.99968"
-                stroke="#3A3D42"
-                stroke-opacity="0.8"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            Transfer
+              <svg
+                width="28"
+                height="28"
+                viewBox="-5 1 28 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 18.1663V1.83301"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1.83333 9.99968L10 1.83301L18.1667 9.99968"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              Transfer
+            </Link>
           </li>
+
+          <li>
+            <Link
+              href={"/dashboard/topup"}
+              className={`${
+                currentRoute === "/dashboard/topup"
+                  ? style.active
+                  : style.inactive
+              }  border-l-4 px-6 my-5 flex items-center gap-4 transition-colors hover:text-primary-focus   hover:stroke-primary-focus`}
+            >
+              <svg
+                width="28"
+                height="28"
+                viewBox="-7 0 28 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 1.83301V18.1663"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1.83301 10H18.1663"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              Topup
+            </Link>
+          </li>
+
           <li>
             <Link
               href={"/dashboard/profile"}
-              className="border-white border-l-4 px-6 my-5 flex items-center gap-4"
+              className={`${
+                currentRoute === "/dashboard/profile"
+                  ? style.active
+                  : style.inactive
+              }  border-l-4 px-6 my-5 flex items-center gap-4 transition-colors hover:text-primary-focus   hover:stroke-primary-focus`}
             >
               <svg
                 width="28"
@@ -85,19 +143,17 @@ function Sidebar() {
               >
                 <path
                   d="M20.3333 22.5V20.1667C20.3333 18.929 19.8417 17.742 18.9665 16.8668C18.0913 15.9917 16.9043 15.5 15.6667 15.5H6.33334C5.09566 15.5 3.90868 15.9917 3.03351 16.8668C2.15834 17.742 1.66667 18.929 1.66667 20.1667V22.5"
-                  stroke="#3A3D42"
-                  stroke-opacity="0.8"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M11 10.8333C13.5773 10.8333 15.6667 8.744 15.6667 6.16667C15.6667 3.58934 13.5773 1.5 11 1.5C8.42267 1.5 6.33333 3.58934 6.33333 6.16667C6.33333 8.744 8.42267 10.8333 11 10.8333Z"
-                  stroke="#3A3D42"
-                  stroke-opacity="0.8"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeOpacity="0.8"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
               Profile
